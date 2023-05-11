ca635cfc19b95270c2a990ae8de32896af6670bf
Adam Retter <adam.retter@googlemail.com>
2023-05-11 17:49:19 +0200

    TEMP annotate problems with test instantiation of ApiService as a Resource

--- src/test/scala/uk/gov/nationalarchives/ApiServiceSpec.scala
+++ src/test/scala/uk/gov/nationalarchives/ApiServiceSpec.scala
@@ -12,13 +12,16 @@ class ApiServiceSpec extends FixtureAsyncWordSpec with AsyncIOSpec with CatsReso
 
   private val service = new ApiService
 
-  private val task = service.start
+  private val task: IO[Unit] = service.start
+
+  // TODO(AR) what about `IO.interruptible` - see https://typelevel.org/cats-effect/docs/tutorial#dealing-with-cancelation
 
   override val resource: Resource[IO, IO[OutcomeIO[Unit]]] = task.onCancel(service.stop()).background
 
+
   "cats resource specifications" should {
-    "run a resource modification" in { _ =>
-      resource.surround {
+    "run a resource modification" in { res =>   // resource is started 1st time here when it is injected.
+      resource.surround {   // resources will be started a 2nd time here when the test is evaluated (as it is explicitly converted into an IO[Unit] here)
         for {
           _ <- IO.sleep(5.seconds)
           _ <- IO.println("Test 1")
@@ -27,24 +30,24 @@ class ApiServiceSpec extends FixtureAsyncWordSpec with AsyncIOSpec with CatsReso
       }
     }
 
-    "be shared between tests" in { _ =>
-      resource.surround {
-        for {
-          _ <- IO.sleep(5.seconds)
-          _ <- IO.println("Test 2")
-          res <- IO.pure(2 + 3).asserting(_ mustBe 5)
-        } yield res
-      }
-    }
-
-    "be shared between three" in { _ =>
-      resource.surround {
-        for {
-          _ <- IO.sleep(5.seconds)
-          _ <- IO.println("Test 3")
-          res <- IO.pure(2 + 3).asserting(_ mustBe 5)
-        } yield res
-      }
-    }
+//    "be shared between tests" in { _ =>
+//      resource.surround {
+//        for {
+//          _ <- IO.sleep(5.seconds)
+//          _ <- IO.println("Test 2")
+//          res <- IO.pure(2 + 3).asserting(_ mustBe 5)
+//        } yield res
+//      }
+//    }
+//
+//    "be shared between three" in { _ =>
+//      resource.surround {
+//        for {
+//          _ <- IO.sleep(5.seconds)
+//          _ <- IO.println("Test 3")
+//          res <- IO.pure(2 + 3).asserting(_ mustBe 5)
+//        } yield res
+//      }
+//    }
   }
 }
