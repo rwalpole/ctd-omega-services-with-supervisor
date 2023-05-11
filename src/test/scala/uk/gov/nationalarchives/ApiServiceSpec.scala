13083cdca8b9a249a7ec3d8e71b36f8457225819
Adam Retter <adam.retter@googlemail.com>
2023-05-11 21:32:44 +0200

    TEMP annotate problems with test instantiation of ApiService as a Resource

--- src/test/scala/uk/gov/nationalarchives/ApiServiceSpec.scala
+++ src/test/scala/uk/gov/nationalarchives/ApiServiceSpec.scala
@@ -11,30 +11,28 @@ class ApiServiceSpec extends FixtureAsyncWordSpec with AsyncIOSpec with CatsReso
 
   private val service = new ApiService
 
-  private val task = service.start
+  private val task: IO[Unit] = service.start.onCancel(service.stop()) // TODO(AR) the `service.stop()` is never run due to a known bug - https://github.com/typelevel/cats-effect-testing/issues/300#issuecomment-1544442344
 
-  override val resource: Resource[IO, Supervisor[IO]] = {
-    task.onCancel(service.stop()) // this is never run
-    Supervisor[IO](await = false) //.onCancel(service.stop()) // not allowed here
-  }
+  override val resource: Resource[IO, Supervisor[IO]] = Supervisor[IO](await = false)
 
   "cats resource specifications" should {
     "run a resource modification" in {supervisor =>
       for {
-        fiber <- supervisor.supervise[Unit](task.foreverM)
+        //fiber <- supervisor.supervise[Unit](task.foreverM)    // TODO(AR) the `foreverM` is not needed here as `task` i.e. `service.start` will run forever
+        fiber <- supervisor.supervise[Unit](task)
         _ <- IO.sleep(5.seconds)
         res <- IO.pure(2 + 3).asserting(_ mustBe 5)
-        _ <- fiber.cancel
+//        _ <- fiber.cancel         // TODO(AR) I don't think this is required because the Release of the Resource will stop the supervisor, which will stop all fibers that are supervised
       } yield res
     }
 
-    "be shared between tests" in { supervisor =>
-      for {
-        fiber <- supervisor.supervise[Unit](task.foreverM)
-        _ <- IO.sleep(5.seconds)
-        res <- IO.pure(2 + 3).asserting(_ mustBe 5)
-        _ <- fiber.cancel
-      } yield res
-    }
+//    "be shared between tests" in { supervisor =>
+//      for {
+//        fiber <- supervisor.supervise[Unit](task.foreverM)
+//        _ <- IO.sleep(5.seconds)
+//        res <- IO.pure(2 + 3).asserting(_ mustBe 5)
+//        _ <- fiber.cancel
+//      } yield res
+//    }
   }
 }
